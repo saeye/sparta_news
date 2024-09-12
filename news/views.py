7653d583526c35fe3e5570d985991229842bce1c
qwerrrqw <latiger95@gmail.com>
2024-09-12 16:12:56 +0900

    commit message
    news/views
    NewsListView에 get_queryset 함수로 검색 기능 구현
    search_title_content_query 검색어가 포함된 내용과 제목 모두 검색 할수 있음
    title_query, content_query, author_query는 제목, 내용, 저자 각각 검색
    가능

--- news/views.py
+++ news/views.py
@@ -6,6 +6,8 @@ from .serializers import NewsSerializer, CategorySerializer, NewsDetailSerialize
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
 # Create your views here.
 
 # 카테고리 생성 admin user만 카테고리 생성가능
@@ -22,8 +24,32 @@ class CategoryView(CreateAPIView):
 class NewsListView(ListCreateAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
-    # 로그인 없이 조회가능, 게시글 작성시 로그인 필수
+    # 로그인 없이 조회 가능, 게시글 작성 시 로그인 필수
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        queryset = News.objects.all()
+        search_title_content_query = self.request.query_params.get('q', None) # 통합 검색 /api/news/?q=제목+내용
+        title_query = self.request.query_params.get('title', None)  # 제목만 검색: /api/news/?title=제목
+        content_query = self.request.query_params.get('content', None) # 내용만 검색: /api/news/?content=내용
+        author_query = self.request.query_params.get('author', None) # 저자만 검색: /api/news/?author=저자
+        
+        # 제목, 내용 모두 검색
+        if search_title_content_query: 
+            queryset = queryset.filter(
+                Q(title__icontains=search_title_content_query) |
+                Q(content__icontains=search_title_content_query)
+                )
+        # 제목, 내용, 저자 각각 따로 검색
+        else: 
+            if title_query:
+                queryset = queryset.filter(title__icontains=title_query)
+            if content_query:
+                queryset = queryset.filter(content__icontains=content_query)
+            if author_query:
+                queryset = queryset.filter(author__username__icontains=author_query)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
