5d1dea9682b1bc851b3a72414d2617813ada33cf
kimwoolina <rladnfls0500@gmail.com>
2024-09-12 15:50:37 +0900

    feat: login, logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
-urlpatterns = [
-    # JWT 토큰 관련 URL
-    path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    
-    # 사용자 생성 관련 URL
+urlpatterns = [   
     path("signup/", views.UserCreateView.as_view()),
+    path("signin/", views.SigninView.as_view()),
+    path("signout/", views.SignoutView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),  
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,11 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
-from .validators import validate_user_data  # validators.py에서 가져오기
+from .validators import validate_user_data # validators.py에서 가져오기
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
 
 class UserCreateView(APIView):
     def post(self, request):
@@ -19,6 +23,39 @@ class UserCreateView(APIView):
         
         serializer = UserSerializer(user)
         return Response({"message": "가입 완료👌", "data": serializer.data}, status=status.HTTP_201_CREATED)
-    
 
     
+# 로그인
+class SigninView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response({"error": "Username or password is incorrect."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # 인증 후 토큰 발급
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "access_token": str(refresh.access_token),
+            "refresh_token": str(refresh)
+        }, status=status.HTTP_200_OK)
+
+
+# 로그아웃
+class SignoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        refresh_token_str = request.data.get("refresh_token")
+        refresh_token = RefreshToken(refresh_token_str)
+
+        try:
+            refresh_token.check_blacklist()
+        except Exception:
+            return Response({"error": "The token is invalid."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        refresh_token.blacklist()
+        return Response({"message": "You have been logged out."}, status=status.HTTP_200_OK)
