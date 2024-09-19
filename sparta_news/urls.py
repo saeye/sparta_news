297fde6d8f25c15e6af11367c9cd49e63164d693
gr22nsky <gr2nsky@gmail.com>
2024-09-19 10:49:40 +0900

    pull

--- sparta_news/urls.py
+++ sparta_news/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('news/', include('news.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/news/', include('news.urls')),
 ]
