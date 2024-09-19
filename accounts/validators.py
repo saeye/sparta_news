368c1b766653bf444d029967a8429d10bff77e2b
saeye <saeye42@gmail.com>
2024-09-20 06:28:12 +0900

    feature: 회원가입 필수필드 코드수정

--- accounts/validators.py
+++ accounts/validators.py
@@ -2,35 +2,37 @@ from .models import User
 import re
 
 def validate_user_data(user_data):
+    # 필수 입력 값 확인
+    required_fields = ['username', 'password', 'email']
+    missing_fields = [field for field in required_fields if not user_data.get(field)]
+
+    # 누락된 필드가 있을 경우 각 필드에 맞는 메시지 반환
+    if missing_fields:
+        return f"{', '.join(missing_fields)}를(을) 입력하세요."
+
     username = user_data.get("username")
     password = user_data.get("password")
     email = user_data.get("email")
 
     email_list = ["naver.com", "gmail.com", "daum.net"]
 
+    # username 중복 확인
     if User.objects.filter(username=username).exists():
         return "이미 존재하는 username이에요🫠"
-    elif username is None:
-        return "username을 입력하세요"
     
+    # email 중복 및 형식 확인
     if User.objects.filter(email=email).exists():
         return "이미 존재하는 email이에요"
-    elif email is None:
-        return "email을 입력하세요"
     elif "@" not in email:
         return "email 형식에 맞춰주세요."
-        
     elif email.split("@")[1] not in email_list:
-        
         return f"허용되지 않은 email 주소입니다.\n{email_list} 중 하나만 입력하세요"
     
+    # 비밀번호 길이 및 형식 확인
     if len(password) < 8:
         return "비밀번호는 8자 이상이어야 해요🫠"
     
-    elif password is None:
-        return "비밀번호를 입력하세요"
     return None
-    
 
 
 def changepasswordValidation(pwd):
